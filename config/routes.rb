6a3167e49b9fe2da7f6c6ca8dd502d8a0f6d059f
slavam1 <mwm1955@gmail.com>
2016-01-28 15:22:40 +0000

    Layout and routes changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'categories#index'
+  get 'signup'  => 'users#new'
+
   resources :categories
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
